ec53cf279a160303d2d7628444b76c397e7cf3b2
Swati Matre <matreswati423@gmail.com>
2026-06-14 21:15:15 +0530

    last changes

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = ({ children }) => {
-  const url = "http://localhost:4000";
+  const url = "https://food-del-backend-p3jv.onrender.com";
   const currency = "$";
 
   const getItem = (key) => {
